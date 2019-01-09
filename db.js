00f1296831d7eeab48a08be36af74346671d6010
GUNNER9285 <GUNNER9285@gmail.com>
2019-01-09 12:09:20 +0700

    fix uri mongodb (2)

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 var mongojs = require('mongojs');
 
-var databaseUrl = 'mongodb://localhost/tgr2019test';
+var databaseUrl = 'mongodb://202.139.192.79/tgr2019test';
 var collections = ['test'];
 var option =  {"auth":{"user":"canet", "password":"tesatopgungtbnb"}};
 var connect = mongojs(databaseUrl, collections, option);
